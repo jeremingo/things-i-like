049d69cb552562d35e1eeb7a8edd88c7057a4934
jeremingo <58335558+jeremingo@users.noreply.github.com>
2025-03-23 12:36:49 +0200

    gh-70 Add Bootstrap

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -10,24 +10,27 @@ import EditUser from './EditUser'
 import PostPage from './PostPage'
 import EditComment from './EditComment'
 import NewComment from './NewComment'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/user/:userId' element={<Profile />} />
-        <Route path='/new-post' element={<NewPost />} />
-        <Route path='/edit-post/:postId' element={<EditPost />} />
-        <Route path='/settings' element={<EditUser />} />
-        <Route path='/post/:postId' element={<PostPage />} />
-        <Route path='/edit-comment/:commentId' element={<EditComment />} />
-        <Route path='/add-comment/:postId' element={<NewComment />} />
-      </Routes>
+      <div style={{ paddingTop: '56px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/user/:userId' element={<Profile />} />
+          <Route path='/new-post' element={<NewPost />} />
+          <Route path='/edit-post/:postId' element={<EditPost />} />
+          <Route path='/settings' element={<EditUser />} />
+          <Route path='/post/:postId' element={<PostPage />} />
+          <Route path='/edit-comment/:commentId' element={<EditComment />} />
+          <Route path='/add-comment/:postId' element={<NewComment />} />
+        </Routes>
+      </div>
     </>
   )
 }
