24baa73d73ae6dd6daa131a7da4c12107eb3fa98
jeremingo <58335558+jeremingo@users.noreply.github.com>
2025-03-23 11:55:12 +0200

    gh-50 Add Photo to Post

--- apps/frontend/src/PostForm.tsx
+++ apps/frontend/src/PostForm.tsx
@@ -1,15 +1,17 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import authService from "./services/auth-service";
 import { useNavigate } from "react-router-dom";
 import { Post } from "@things-i-like/post";
+import { uploadPhoto } from "./services/file-service";
 
 const schema = z.object({
   title: z.string()
     .nonempty('Title is required'),
-  content: z.string()
+  content: z.string(),
+  photo: z.string().optional(),
 })
 
 export type PostFormData = z.infer<typeof schema>;
@@ -26,6 +28,8 @@ const PostForm: React.FC<PostFormProps> = ({ onSubmit, initialData, action }) =>
     mode: 'onChange',
     defaultValues: initialData,
   });
+  const [imgSrc, setImgSrc] = useState<File>()
+  const fileInputRef = useRef<HTMLInputElement>(null)
 
   const navigate = useNavigate();
   
@@ -35,9 +39,45 @@ const PostForm: React.FC<PostFormProps> = ({ onSubmit, initialData, action }) =>
     }
   }, [navigate]);
 
+  const imgSelected = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    if (e.target.files && e.target.files.length > 0) {
+      setImgSrc(e.target.files[0])
+    }
+  }
+  const selectImg = () => {
+    console.log("Selecting image...")
+    fileInputRef.current?.click()
+  }
+
+  const onSubmitExtended = (data: PostFormData) =>{
+    if (!imgSrc) {
+      onSubmit(data)
+      return
+    } else {
+      uploadPhoto(imgSrc as File).then((url) => {
+        onSubmit({
+          ...data,
+          photo: url
+        });
+      })
+    }
+  };
+
   return (
     <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
-      <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+      <form onSubmit={handleSubmit(onSubmitExtended)} style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+        <label htmlFor="file" style={{ fontWeight: 'bold' }}>Photo</label>
+        <div className="d-flex justify-content-center position-relative">
+          {(imgSrc || initialData?.photo) && 
+          <img src={imgSrc ? URL.createObjectURL(imgSrc) : initialData?.photo} style={{ height: "230px", width: "230px" }} className="img-fluid" />}
+          <button type="button" className="btn position-absolute bottom-0 end-0" onClick={selectImg}>
+            Select
+          </button>
+        </div>
+
+        <input style={{ display: "none" }} ref={fileInputRef} type="file" onChange={imgSelected}></input>
+
         <label htmlFor="title" style={{ fontWeight: 'bold' }}>Title</label>
         <input {...register('title')} type="text" id="title" />
         {formState.errors.title && <p>{formState.errors.title?.message}</p>}
--- libs/post/src/post.ts
+++ libs/post/src/post.ts
@@ -4,6 +4,7 @@ export interface Post {
   userId: ObjectId;
   title: string;
   content?: string;
+  photo?: string;
   likeCount: number;
   commentCount: number;
   _id?: ObjectId;
